44c81a15767e512dc69243a78c1150af5caf0be1
Svetlana Levinsohn <svetlana.levinsohn@gmail.com>
2016-11-20 16:00:17 -0800

    login test added

---
+++ python-example/logintest.py
@@ -0,0 +1,21 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome()
+    request.addfinalizer(wd.quit)
+    return wd
+
+def test_login(driver):
+    driver.get("http://localhost/litecart/admin/login.php")
+
+    driver.find_element_by_name("username").send_keys("admin")
+    driver.find_element_by_name("password").send_keys("admin")
+    driver.find_element_by_name("login").click()
+    # making sure user is logged in
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH,"//a[@title='Logout']")))
+
