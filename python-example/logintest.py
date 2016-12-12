45d09b5d0914d35019ab151b05f697a4459b30b2
Svetlana Levinsohn <svetlana.levinsohn@gmail.com>
2016-12-11 23:41:59 -0800

    login updated

--- python-example/logintest.py
+++ python-example/logintest.py
@@ -4,15 +4,16 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 @pytest.fixture
 def driver(request):
     wd = webdriver.Chrome()
     request.addfinalizer(wd.quit)
     return wd
 
-def test_login(driver):
-    driver.get("http://localhost/litecart/admin/login.php")
 
+def test_log_in(driver):
+    driver.get("http://localhost/litecart/admin/login.php")
     driver.find_element_by_name("username").send_keys("admin")
     driver.find_element_by_name("password").send_keys("admin")
     driver.find_element_by_name("login").click()
